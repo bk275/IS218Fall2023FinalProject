98f4c1536884178c90b33a2438ea01b5047c9ab8
Ben Kustanovich <bk275@njit.edu>
2023-12-16 17:38:46 -0500

    started a new website

---
+++ finalproject/pages/index.js
@@ -0,0 +1,13 @@
+// pages/index.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to My Empty Next.js Project</h1>
+      {/* Add your content here */}
+    </div>
+  );
+};
+
+export default Home;
